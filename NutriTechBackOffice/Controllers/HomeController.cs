8f3207d80328c0f8d66787c22b75e16efb63ef0d
ivan hribernik <cihribernik@gmail.com>
2021-08-28 00:51:25 -0300

    Add project files.

---
+++ NutriTechBackOffice/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NutriTechBackOffice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<HelloMessage> Get()
+        {
+            HelloMessage message = new HelloMessage
+            {
+                MessageText = "Hello my Dear!",
+                UserName = "pepito"
+            };
+            return message;
+        }
+    }
+    public class HelloMessage
+    {
+        public string MessageText { get; set; }
+        public string UserName { get; set; }
+    }
+}
