da4e0143743f4cb7e0accb2dbe33ba41042bd6c2
ivan hribernik <cihribernik@gmail.com>
2021-09-08 03:16:14 -0300

    tres tests que miseria

---
+++ NutriTechBackOffice.Test.Controllers/Fixtures/FirestoreTestFixture.cs
@@ -0,0 +1,49 @@
+﻿using Google.Api.Gax;
+using Google.Cloud.Firestore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace NutriTechBackOffice.Test.Controllers.Fixtures
+{
+    public class FirestoreTestFixture
+    {
+        public FirestoreTestFixture()
+        {
+            // si si lo se esto esta horrible pero bueno no se me ocurrio de otra manera
+            string filepath = @"C:\Users\ivan_\.ssh\service-account-file.json";
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", filepath);
+            DB = CreateDbInstance();
+            Clean(DB.Collection("Users"));
+            Clean(DB.Collection("Roles"));
+
+        }
+        public FirestoreDb DB { get; }
+        public IConfiguration Configuration { get; } = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", true)
+        .AddJsonFile(@"C:\Users\ivan_\.ssh\service-account-file.json", true)
+        .AddEnvironmentVariables()
+        .AddUserSecrets<FirestoreTestFixture>()
+        .Build();
+
+        private FirestoreDb CreateDbInstance()
+        {
+            return new FirestoreDbBuilder
+            {
+                ProjectId = Configuration["project_id"],
+                EmulatorDetection = EmulatorDetection.EmulatorOnly
+            }.Build();
+        }
+        private void Clean(CollectionReference collection)
+        {
+            var snapShot = collection.GetSnapshotAsync().GetAwaiter().GetResult();
+            foreach (var doc in snapShot.Documents)
+            {
+                doc.Reference.DeleteAsync().GetAwaiter().GetResult();
+            }
+        }
+    }
+}
---
+++ NutriTechBackOffice.Test.Controllers/UserControllerTest.cs
@@ -0,0 +1,74 @@
+﻿using NutriTechBackOffice.Controllers;
+using NutriTechBackOffice.Services;
+using NutriTechBackOffice.Test.Controllers.Fixtures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Sdk;
+using Moq;
+using NutriTechBackOffice.Services.Users.Queries;
+using MediatR;
+using FluentAssertions;
+using NutriTechBackOffice.Domain.Entities;
+using System.Threading;
+using NutriTechBackOffice.Services.Users.Commands;
+using NutriTechBackOffice.Services.Users.Forms;
+
+namespace NutriTechBackOffice.Test.Controllers
+{
+    public class UserControllerTest : IClassFixture<FirestoreTestFixture>
+    {
+        private readonly FirestoreTestFixture _fixture;
+        private readonly FirestoreHelper _context;
+        private readonly Mock<IMediator> mediatrMock;
+
+
+        UserController _userController;
+
+        public UserControllerTest(FirestoreTestFixture fixture)
+        {
+            this._fixture = fixture;
+            this.mediatrMock = new Mock<IMediator>();
+        }
+
+        [Fact]
+        public async Task GetAllUsers()
+        {
+            mediatrMock.Setup(m => m.Send(It.IsAny<GetUsersQuery>(), It.IsAny<CancellationToken>()));
+            _userController = new UserController(mediatrMock.Object);
+            var result = _userController.GetUsers();
+            result.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task InsertUser()
+        {
+            mediatrMock.Setup(m => m.Send(It.IsAny<InsertUserCommand>(), It.IsAny<CancellationToken>()));
+            _userController = new UserController(mediatrMock.Object);
+            var insertForm = new InsertUserForm() { 
+                Nombre = "prueba ivan",
+                Apellido = "hribernik",
+                Email = "algo@gmail.com",
+                Password ="hola1234",
+                Rol = new Role()
+                {
+                    Nombre = "admin",
+                    Descripcion = "admin que te borra la base en produccion porque estaba armando tests a la madrugada"
+                }
+            };
+            var result = _userController.PostAsync(insertForm);
+            result.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task GetUserById_retunsNotNull()
+        {
+            mediatrMock.Setup(m => m.Send(It.IsAny<GetUserByIdQuery>(), It.IsAny<CancellationToken>()));
+            _userController = new UserController(mediatrMock.Object);
+            var result = _userController.Get("pepe");
+            result.Should().NotBeNull();
+        }
+    }
+}
