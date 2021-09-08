22a8e37d4c55f77d7f306314ebb9b2e856f43009
ivan hribernik <cihribernik@gmail.com>
2021-09-08 03:22:37 -0300

    se modifico mas el commit

--- NutriTechBackOffice.Test.Controllers/Fixtures/FirestoreTestFixture.cs
+++ NutriTechBackOffice.Test.Controllers/Fixtures/FirestoreTestFixture.cs
@@ -15,6 +15,7 @@ namespace NutriTechBackOffice.Test.Controllers.Fixtures
             // si si lo se esto esta horrible pero bueno no se me ocurrio de otra manera
             string filepath = @"C:\Users\ivan_\.ssh\service-account-file.json";
             Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", filepath);
+            Environment.SetEnvironmentVariable("FIRESTORE_EMULATOR_HOST", "localhost:8080");
             DB = CreateDbInstance();
             Clean(DB.Collection("Users"));
             Clean(DB.Collection("Roles"));
