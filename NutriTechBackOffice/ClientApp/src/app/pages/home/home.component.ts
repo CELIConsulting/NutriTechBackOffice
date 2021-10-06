acc0f28e372069bb925efaaeedf000b53d9657f5
chrisXIII <cristian_a_rojas@hotmail.com>
2021-10-06 19:43:06 -0300

    home

--- NutriTechBackOffice/ClientApp/src/app/pages/home/home.component.ts
+++ NutriTechBackOffice/ClientApp/src/app/pages/home/home.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { UsersService } from 'src/app/services/users.service';
+import { User } from '../../interfaces/user';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { userInfo } from 'os';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +13,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['nombre', 'apellido'];
+  dataSource: MatTableDataSource<User>;
+  pacientes: User[];
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
+  constructor(private usersService: UsersService) { }
 
   ngOnInit() {
+    this.cargarGrilla();
+  }
+
+  cargarGrilla() {
+    var i=0;
+    this.usersService.getPatients()
+      .subscribe(
+        patient => {        
+          if(/*patient[i].planAsignado!=null*/true){
+            this.pacientes = patient;
+            this.dataSource = new MatTableDataSource(this.pacientes);
+  
+            this.dataSource.paginator = this.paginator;
+            this.dataSource.sort = this.sort;
+            i++;
+          }         
+        },
+        error => {
+          console.error("No se pudo obtener a los pacientes con planes")
+          console.log(error)
+        });
+
   }
 
 }
+
+
