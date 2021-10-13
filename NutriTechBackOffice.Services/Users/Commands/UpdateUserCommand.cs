e9ac2d39136f173287d96f244f8e397cd17db979
Lucia <56481679+LU0390@users.noreply.github.com>
2021-10-12 21:29:06 -0300

    Modificacion_usuarios

---
+++ NutriTechBackOffice.Services/Users/Commands/UpdateUserCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using NutriTechBackOffice.Domain.Entities;
+using NutriTechBackOffice.Services.Users.Forms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NutriTechBackOffice.Services.Users.Commands
+{
+    public class UpdateUserCommand : IRequest<User>
+    {
+        public InsertUserForm UserForm{ get; }
+        public string Email { get; set; }
+
+        public UpdateUserCommand(string email, InsertUserForm userForm)
+        {
+            UserForm = userForm;
+            Email = email;
+        }
+
+    }
+}
--- NutriTechBackOffice/ClientApp/src/app/pages/modificar-usuarios/modificar-usuarios.component.ts
+++ NutriTechBackOffice/ClientApp/src/app/pages/modificar-usuarios/modificar-usuarios.component.ts
@@ -1,5 +1,14 @@
-import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Role } from '../../interfaces/role';
+import { User } from '../../interfaces/user';
+import { UserForm } from '../../interfaces/user-form';
+import { RolesService } from '../../services/roles.service';
+import { UsersService } from '../../services/users.service';
+import { PopUpComponent } from '../../components/pop-up/pop-up.component';
+import { MatDialog } from '@angular/material'
+
 
 
 @Component({
@@ -8,11 +17,85 @@ import { ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['./modificar-usuarios.component.css']
 })
 export class ModificarUsuariosComponent implements OnInit {
+  usuarioModificar: User;
+  rol: Role;
+  rolSeleccionado: string;
+  
 
-  constructor(private activatedRoute: ActivatedRoute) { }
-
-  ngOnInit() {
-    console.log(this.activatedRoute.snapshot.params.email)
+  userModificacionForm = this.fb.group({
+    firstName: [null, Validators.required],
+    lastName: [null, Validators.required],
+    email: [null],
+    password: [null, Validators.required],
+    rol: [null, Validators.required],
+  });
+  roles: any[] = [];
+  constructor(private fb: FormBuilder, private roleService: RolesService, private usersService: UsersService, public dialog: MatDialog, private activatedRoute: ActivatedRoute) {
+    this.fillRoles();
   }
 
+
+  fillRoles() {
+    this.roleService.getRoles().subscribe(
+      data => {
+        this.roles = data.map(item => { return { nombre: item.nombre } })
+      },
+      err => {
+        console.log(err)
+      }
+    )
+  }
+
+
+  ngOnInit() {
+    this.cargarUsuario();
+  
+   
+  }
+
+  cargarUsuario() {
+    var emailUsuario = this.activatedRoute.snapshot.params.email;
+    this.usersService.getUserById(emailUsuario).subscribe(
+      data => {
+        this.usuarioModificar = data;
+        this.userModificacionForm.controls.firstName.setValue(this.usuarioModificar.nombre);
+        this.userModificacionForm.controls.lastName.setValue(this.usuarioModificar.apellido);
+        this.userModificacionForm.controls.email.setValue(this.usuarioModificar.email);
+        this.userModificacionForm.controls.password.setValue(this.usuarioModificar.password);
+        this.rolSeleccionado= this.usuarioModificar.rol;
+    
+      },
+      err => {
+
+      }
+    )
+  }
+  onSubmit() {
+    if (this.userModificacionForm.valid) {
+      let user: UserForm = {
+        'Nombre': this.userModificacionForm.value['firstName'],
+        'Apellido': this.userModificacionForm.value['lastName'],
+        'Email': this.userModificacionForm.value['email'],
+        'Password': this.userModificacionForm.value['password'],
+        'Rol': this.userModificacionForm.value['rol'].nombre,
+        //TODO: Agregar inputs al form
+        'FechaNacimiento': null,
+        'Telefono': null,
+      };
+
+      //servicio de actualizacion
+      this.usersService.updateUser(user.Email, user).subscribe(
+        data => {
+          this.dialog.open(PopUpComponent, { data: { title: "Listo!", message: "El usuario fue correctamente modificado." } });
+
+        },
+        err => {
+          this.dialog.open(PopUpComponent, { data: { title: "Ups hubo un error!", message: "No se pudo modificar el usuario." } });
+
+        }
+
+      )
+      
+    }
+  }
 }
