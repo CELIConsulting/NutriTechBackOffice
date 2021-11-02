c90434757a31e4d51887997fe77f64b3edd4c52b
Ivan Hribernik <cihribernik@gmail.com>
2021-11-02 17:46:21 -0300

    ajuste de claims c2

--- NutriTechBackOffice/ClientApp/src/app/services/auth.service.ts
+++ NutriTechBackOffice/ClientApp/src/app/services/auth.service.ts
@@ -7,79 +7,25 @@ import { Subject } from "rxjs";
   providedIn: "root",
 })
 export class AuthService {
+  public userClaims: any;
+  public userClaims$ = new Subject<any>();
 
-  uid: string = null;
-  user: User = null;
-  claims: any = {};
-  isAdmin = false;
-  isNutrittionist = false;
+  constructor(private afAuth: AngularFireAuth) { }
 
-  isLoggedInSubject = new Subject<boolean>();
-  userSubject = new Subject();
-  claimsSubject = new Subject();
-  isAdminSubject = new Subject<boolean>();
-  isNuttritionistSubject = new Subject<boolean>();
-
-  constructor(private afAuth: AngularFireAuth) {
-
-    // the only subsription to authState
-    this.afAuth.authState
-      .subscribe(
-        authUser => {
-
-          if (authUser) { // logged in
-            this.isLoggedInSubject.next(true);
-            this.uid = authUser.uid;
-
-            this.claims = authUser.getIdTokenResult()
-              .then(idTokenResult => {
-                this.claims = idTokenResult.claims;
-                this.isAdmin = this.hasClaim('Admin');
-                this.isAdminSubject.next(this.isAdmin);
-                this.isAdmin = this.hasClaim('Nutricionista');
-                this.isNuttritionistSubject.next(this.isNutrittionist);
-                this.claimsSubject.next(this.claims);
-              });
-          }
-          else { // logged out
-            console.log('Auth Service says: no User is logged in.');
-          }
+  getUserClaims(): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      this.afAuth.auth.onAuthStateChanged((user) => {
+        if (!!user) {
+          this.setUserClaims(user);
+          resolve(user);
+        } else {
+          reject("No user logged in");
         }
-      );
+      });
+    });
   }
-
-  isAdminUser() {
-    return this.isAdmin;
+  setUserClaims(user: any): void {
+    this.userClaims = user;
+    this.userClaims$.next(user);
   }
-
-  isNutrittionistUser() {
-    return this.isNutrittionist;
-  }
-
-  login(email, password): Promise<any> {
-    return this.afAuth.auth.signInWithEmailAndPassword(email, password);
-  }
-
-  logout() {
-    this.resetState();
-    this.afAuth.auth.signOut();
-    console.log('User just signed out.');
-  }
-
-  hasClaim(claim): boolean {
-    return !!this.claims[claim];
-  }
-
-  resetState() {
-    this.uid = null;
-    this.claims = {};
-    this.user = null;
-    this.isAdmin = false;
-
-    this.isLoggedInSubject.next(false);
-    this.isAdminSubject.next(false);
-    this.claimsSubject.next(this.claims);
-    this.userSubject.next(this.user);
-  }
-
 }
