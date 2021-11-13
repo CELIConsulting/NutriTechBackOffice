d8e1822e69b4716d1599460a0a82593bb77416a3
Lucia <56481679+LU0390@users.noreply.github.com>
2021-11-13 12:29:49 -0300

    progreso_paciente

---
+++ NutriTechBackOffice/ClientApp/src/app/pages/progreso-paciente/progreso-paciente -DataGrafic.ts
@@ -0,0 +1,36 @@
+export var multi = [
+  {
+    name: 'Progreso del peso',
+    series: [
+      {
+        name: '2021-01-01',
+        value: 100,
+      },
+      {
+        name: '2021-05-01',
+        value: 80,
+      },
+      {
+        name: '2021-06-01',
+        value: 79,
+      },
+      {
+        name: '2021-07-01',
+        value: 85,
+      },
+      {
+        name: '2021-08-01',
+        value: 87,
+      },
+      {
+        name: '2021-09-01',
+        value: 85,
+      },
+
+      {
+        name: '2021-11-01',
+        value: 70,
+      },
+    ],
+  },
+];
--- NutriTechBackOffice/ClientApp/src/app/pages/progreso-paciente/progreso-paciente.component.ts
+++ NutriTechBackOffice/ClientApp/src/app/pages/progreso-paciente/progreso-paciente.component.ts
@@ -4,7 +4,7 @@ import { UsersService } from 'src/app/services/users.service';
 import { User } from '../../interfaces/user';
 import { ActivatedRoute, Params, Route, Router } from '@angular/router';
 import { LoadingSpinnerService } from '../../services/loading-spinner.service';
-
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-progreso-paciente',
@@ -19,10 +19,28 @@ export class ProgresoPacienteComponent implements OnInit {
   paciente: User[];
   loading$ = this.loader.loading$;
 
-  constructor(private usersService: UsersService, private loader: LoadingSpinnerService, private activatedRoute: ActivatedRoute,) { }
+  multi: any[];
+  view: any[] = [700, 300];
+
+  // options
+  legend: boolean = true;
+  showLabels: boolean = true;
+  animations: boolean = true;
+  xAxis: boolean = true;
+  yAxis: boolean = true;
+  showYAxisLabel: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = 'Fecha';
+  yAxisLabel: string = 'Peso';
+  timeline: boolean = true;
+
+  constructor(private usersService: UsersService, private loader: LoadingSpinnerService, private activatedRoute: ActivatedRoute,) {
+  }
 
   ngOnInit() {
     this.getRouteParams();
+    debugger
+    this.cargarDatosGraficos(this.emailParam);
     this.obtenerInformacion(this.emailParam);
   }
 
@@ -30,6 +48,36 @@ export class ProgresoPacienteComponent implements OnInit {
     this.activatedRoute.paramMap.subscribe(parameters => {
       this.emailParam = parameters.get('email');
     })
+  }
+
+  cargarDatosGraficos(email) {
+    this.usersService.getBodyProgress(email).subscribe(
+      listDataGrafic => {
+        debugger
+        Object.assign(this, { listDataGrafic });
+      },
+      error => {
+        debugger
+        console.error("No se puede levantar la informacion")
+        console.log(error)
+      }
+     )
+  }
+
+  colorScheme = {
+    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+  };
+
+  onSelect(data): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
 
 
